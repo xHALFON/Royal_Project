b10eb1d95d04a1c1736f04c58f0928ee089a5d85
Guy Halfon <104530982+xHALFON@users.noreply.github.com>
2023-06-16 16:16:46 +0300

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var mongoose = require("mongoose");
 const connectDB = async() =>{
     try{
-        mongoose.connect("mongodb://127.0.0.1:27017/RoyalDB");
+        mongoose.connect("mongodb+srv://royal-main-db-059fe39a16c:TCA4ezvEMbHAmtFkH96s3DRX7RHNdE@prod-us-central1-2.ih9la.mongodb.net/royal-main-db-059fe39a16c/RoyalDB");
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'Cannot connect db'));
         db.once('open', function(){
